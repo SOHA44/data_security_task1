cbe0c303ca3c4d0c532c3af355fc493b14cf1929
soha <sohasayedrefaei@gmail.com>
2026-03-05 20:27:53 +0200

    auto key decryption done

--- src/main/java/Security/AutoKey.java
+++ src/main/java/Security/AutoKey.java
@@ -3,13 +3,27 @@ package Security;
 public class AutoKey {
     public String analyse(String plainText, String cipherText) {
         // Students should complete this part
+
         return null;
     }
 
     public String decrypt(String cipherText, String key) {
         // Students should complete this part
+        cipherText = cipherText.toLowerCase().replaceAll("[^a-z]", "");
+        key = key.toLowerCase().replaceAll("[^a-z]", "");
+        StringBuilder plain_txt = new StringBuilder();
+        int len = cipherText.length();
+        StringBuilder autoKey = new StringBuilder(key);
+        for (int i=0 ;i<len;i++){
+        int c = cipherText.charAt(i)-'a'; // to get num of char in ascii in cipher
+        int k= autoKey.charAt(i)-'a';// to get num of char in ascii in key
+        int p = (c-k + 26) %26; // p = (c-k +26 )%26 to get num of char in plaintxt
+        char plain  =(char)(p+'a'); // to convert int to char
+         autoKey.append(plain); // key = key + (plain)
+         plain_txt.append(plain);
+        }
 
-        return null;
+        return plain_txt.toString();
     }
 
     public String encrypt(String plainText, String key) {
